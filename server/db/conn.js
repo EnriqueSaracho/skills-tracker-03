d6b7f9f5fd216bbcf10fddf5c7cbe307addace19
Enrique Saracho Felix <esarachof97@gmail.com>
2023-03-08 11:59:03 -0800

    Failed attempts at fixing connection to database problem

--- server/db/conn.js
+++ server/db/conn.js
@@ -10,11 +10,12 @@ var _db;
 module.exports = {
   connectToServer: function (callback) {
     console.log("connectToServer works...");
+    console.log(client);
     client.connect((err, db) => {
       console.log("client.connect works...");
       // Verify we got a good "db" object
       if (db) {
-        _db = db.db("employees");
+        _db = db.db("test");
         console.log("Successfully connected to MongoDB.");
       }
       return callback(err);
